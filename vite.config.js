b7e6a01bd7dc7691b649c9fc4ddde078380d318c
vicmat04 <vicmat04@gmail.com>
2026-04-17 23:31:32 -0500

    fix: compatibilidad con Vercel y base path dinamica

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/phtoboothVicky/',
+  // Si estamos en un GitHub Action, usamos la base del repo, sino la raíz (para Vercel o local).
+  base: process.env.GITHUB_ACTIONS ? '/phtoboothVicky/' : '/',
 })
